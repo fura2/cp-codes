d5d328b8de5cfe1a49e92a56dd38add1b802a881
fura2 <yamazoe0577@gmail.com>
2026-03-29 03:43:42 +0900

    add another solution for ABC451 E

---
+++ atcoder/abc/451/e3.cpp
@@ -0,0 +1,130 @@
+#ifndef TEMPLATE_HPP
+#define TEMPLATE_HPP
+
+#pragma GCC optimize("Ofast")
+#pragma GCC optimize("unroll-loops")
+
+#include <bits/stdc++.h>
+
+#define impl_overload4(a, b, c, d, e, ...) e
+#define impl_overload5(a, b, c, d, e, f, ...) f
+#define impl_overload6(a, b, c, d, e, f, g, ...) g
+#define impl_overload7(a, b, c, d, e, f, g, h, ...) h
+
+// clang-format off
+#define impl_rep4(i, a, b, c) for (int i = (a); i < (b); i += (c))
+#define impl_rep3(i, a, b) impl_rep4(i, a, b, 1)
+#define impl_rep2(i, n) impl_rep3(i, 0, n)
+#define impl_rep1(n) for (int _ = 0; _ < (n); ++_)
+#define rep(...) impl_overload4(__VA_ARGS__, impl_rep4, impl_rep3, impl_rep2, impl_rep1)(__VA_ARGS__)
+
+#define impl_rrep4(i, a, b, c) for (int i = (b) - 1; i >= (a); i -= (c))
+#define impl_rrep3(i, a, b) impl_rrep4(i, a, b, 1)
+#define impl_rrep2(i, n) impl_rrep3(i, 0, n)
+#define rrep(...) impl_overload4(__VA_ARGS__, impl_rrep4, impl_rrep3, impl_rrep2)(__VA_ARGS__)
+// clang-format on
+
+#define all(v) std::begin(v), std::end(v)
+#define rall(v) std::rbegin(v), std::rend(v)
+
+template<typename T>
+constexpr int bit(T x, unsigned int k) {
+  return (x >> k) & 1;
+}
+
+template<typename T>
+constexpr bool chmax(T& a, const T& b) {
+  return a < b ? a = b, true : false;
+}
+template<typename T>
+constexpr bool chmin(T& a, const T& b) {
+  return a > b ? a = b, true : false;
+}
+
+void yesno(bool b) {
+  std::cout << (b ? "Yes" : "No") << "\n";
+}
+void yes() {
+  yesno(true);
+}
+void no() {
+  yesno(false);
+}
+
+struct Setup {
+  Setup() {
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+    std::cout << std::fixed << std::setprecision(11);
+  }
+} setup;
+
+#ifdef LOCAL
+#include "template_local.hpp"
+#else
+#define show(...) ((void)0)
+#endif
+
+using uint = unsigned int;
+using lint = long long;
+using ulint = unsigned long long;
+
+using namespace std;
+
+#endif // TEMPLATE_HPP
+
+using graph = vector<vector<int>>;
+
+void add_undirected_edge(graph& G, int u, int v) {
+  G[u].emplace_back(v);
+  G[v].emplace_back(u);
+}
+
+void add_directed_edge(graph& G, int u, int v) {
+  G[u].emplace_back(v);
+}
+
+int main() {
+  int n;
+  cin >> n;
+  vector d(n, vector<int>(n));
+  rep (u, n) {
+    rep (v, u + 1, n) {
+      cin >> d[u][v];
+      d[v][u] = d[u][v];
+    }
+  }
+
+  graph T(n);
+  rep (u, 1, n) {
+    vector<int> P;
+    rep (p, n)
+      if (p != u && d[0][u] == d[0][p] + d[p][u]) {
+        P.emplace_back(p);
+      }
+    if (P.empty()) {
+      no();
+      return 0;
+    }
+
+    int p = *min_element(all(P),
+                         [&](int p1, int p2) { return d[u][p1] < d[u][p2]; });
+    add_undirected_edge(T, u, p);
+  }
+
+  bool ok = true;
+  auto dfs = [&](auto&& dfs, int u, int p, int sum, int r) -> void {
+    if (d[r][u] != sum) ok = false;
+    if (!ok) return;
+    for (int v: T[u])
+      if (v != p) {
+        dfs(dfs, v, u, sum + d[u][v], r);
+      }
+  };
+  rep (r, n) {
+    dfs(dfs, r, -1, 0, r);
+  }
+  yesno(ok);
+
+  return 0;
+}
