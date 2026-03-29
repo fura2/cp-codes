1e762c9d24fc237724e16a03ac36427e432e6a2d
fura2 <yamazoe0577@gmail.com>
2026-03-30 08:49:52 +0900

    add ARC211 B

---
+++ atcoder/arc/211/b.cpp
@@ -0,0 +1,98 @@
+#ifndef TEMPLATE_HPP
+#define TEMPLATE_HPP
+
+#pragma GCC optimize("Ofast")
+#pragma GCC optimize("unroll-loops")
+
+#include <bits/stdc++.h>
+
+#define impl_overload4(a, b, c, d, e, ...) e
+#define impl_overload5(a, b, c, d, e, f, ...) f
+#define impl_overload6(a, b, c, d, e, f, g, ...) g
+#define impl_overload7(a, b, c, d, e, f, g, h, ...) h
+
+// clang-format off
+#define impl_rep4(i, a, b, c) for (int i = (a); i < (b); i += (c))
+#define impl_rep3(i, a, b) impl_rep4(i, a, b, 1)
+#define impl_rep2(i, n) impl_rep3(i, 0, n)
+#define impl_rep1(n) for (int _ = 0; _ < (n); ++_)
+#define rep(...) impl_overload4(__VA_ARGS__, impl_rep4, impl_rep3, impl_rep2, impl_rep1)(__VA_ARGS__)
+
+#define impl_rrep4(i, a, b, c) for (int i = (b) - 1; i >= (a); i -= (c))
+#define impl_rrep3(i, a, b) impl_rrep4(i, a, b, 1)
+#define impl_rrep2(i, n) impl_rrep3(i, 0, n)
+#define rrep(...) impl_overload4(__VA_ARGS__, impl_rrep4, impl_rrep3, impl_rrep2)(__VA_ARGS__)
+// clang-format on
+
+#define all(v) std::begin(v), std::end(v)
+#define rall(v) std::rbegin(v), std::rend(v)
+
+template<typename T>
+constexpr int bit(T x, unsigned int k) {
+  return (x >> k) & 1;
+}
+
+template<typename T>
+constexpr bool chmax(T& a, const T& b) {
+  return a < b ? a = b, true : false;
+}
+template<typename T>
+constexpr bool chmin(T& a, const T& b) {
+  return a > b ? a = b, true : false;
+}
+
+void yesno(bool b) {
+  std::cout << (b ? "Yes" : "No") << "\n";
+}
+void yes() {
+  yesno(true);
+}
+void no() {
+  yesno(false);
+}
+
+struct Setup {
+  Setup() {
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+    std::cout << std::fixed << std::setprecision(11);
+  }
+} setup;
+
+#ifdef LOCAL
+#include "template_local.hpp"
+#else
+#define show(...) ((void)0)
+#endif
+
+using uint = unsigned int;
+using lint = long long;
+using ulint = unsigned long long;
+
+using namespace std;
+
+#endif // TEMPLATE_HPP
+
+int main() {
+  int x, y, z;
+  cin >> x >> y >> z;
+
+  string s1, s2, s3;
+  if (x == y) {
+    s1 = string(x, '0');
+    s2 = s3 = string(z, '0');
+  }
+  else {
+    s1 = string(x, '0') + string(y, '1');
+    s2 = string(max(x, z), '0');
+    s3 = string(y, '1') + string(z, '0');
+  }
+
+  for (auto s: {s1, s2, s3}) {
+    cout << s.size();
+    for (char c: s) cout << " " << c;
+    cout << "\n";
+  }
+
+  return 0;
+}
