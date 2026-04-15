e7a901ae1bb7026b0f5bafa20c90c956430304ef
fura2 <yamazoe0577@gmail.com>
2026-04-16 02:20:25 +0900

    add AB436 G

---
+++ atcoder/abc/abc436/g.cpp
@@ -0,0 +1,172 @@
+#ifndef TEMPLATE_HPP
+#define TEMPLATE_HPP
+
+#pragma GCC optimize("Ofast")
+#pragma GCC optimize("unroll-loops")
+
+#include <bits/stdc++.h>
+
+// clang-format off
+#define impl_overload4(a, b, c, d, e, ...) e
+
+#define impl_rep4(i, a, b, c) for (int i = (a); i < (b); i += (c))
+#define impl_rep3(i, a, b) impl_rep4(i, a, b, 1)
+#define impl_rep2(i, n) impl_rep3(i, 0, n)
+#define impl_rep1(n) for (int _ = 0; _ < (n); ++_)
+#define rep(...) impl_overload4(__VA_ARGS__, impl_rep4, impl_rep3, impl_rep2, impl_rep1)(__VA_ARGS__)
+
+#define impl_rrep4(i, a, b, c) for (int i = (b) - 1; i >= (a); i -= (c))
+#define impl_rrep3(i, a, b) impl_rrep4(i, a, b, 1)
+#define impl_rrep2(i, n) impl_rrep3(i, 0, n)
+#define rrep(...) impl_overload4(__VA_ARGS__, impl_rrep4, impl_rrep3, impl_rrep2)(__VA_ARGS__)
+
+#define all(v) std::begin(v), std::end(v)
+
+template<typename T>
+constexpr int bit(T x, unsigned int k) { return (x >> k) & 1; }
+
+template<typename T>
+constexpr bool chmax(T& a, const T& b) { return a < b ? a = b, true : false; }
+template<typename T>
+constexpr bool chmin(T& a, const T& b) { return a > b ? a = b, true : false; }
+
+constexpr void yesno(bool b) { std::cout << (b ? "Yes" : "No") << "\n"; }
+constexpr void yes() { yesno(true); }
+constexpr void no() { yesno(false); }
+
+struct Setup {
+  Setup() {
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+    std::cout << std::fixed << std::setprecision(10);
+  }
+} setup;
+// clang-format on
+
+#ifdef LOCAL
+#include "template_local.hpp"
+#else
+#define show(...) ((void)0)
+#endif
+
+using lint = long long;
+
+using namespace std;
+
+#endif // TEMPLATE_HPP
+
+class mint {
+  static const int MOD = 998244353;
+  int x;
+
+ public:
+  mint(): x(0) {}
+  mint(long long y) {
+    x = y % MOD;
+    if (x < 0) x += MOD;
+  }
+
+  mint& operator+=(const mint& m) {
+    x += m.x;
+    if (x >= MOD) x -= MOD;
+    return *this;
+  }
+  mint& operator-=(const mint& m) {
+    x -= m.x;
+    if (x < 0) x += MOD;
+    return *this;
+  }
+  mint& operator*=(const mint& m) {
+    x = (long long)x * m.x % MOD;
+    return *this;
+  }
+  mint& operator/=(const mint& m) {
+    return *this *= inverse(m);
+  }
+  mint operator+(const mint& m) const {
+    return mint(*this) += m;
+  }
+  mint operator-(const mint& m) const {
+    return mint(*this) -= m;
+  }
+  mint operator*(const mint& m) const {
+    return mint(*this) *= m;
+  }
+  mint operator/(const mint& m) const {
+    return mint(*this) /= m;
+  }
+  mint operator-() const {
+    return -x;
+  }
+
+  friend mint inverse(const mint& m) {
+    int a = m.x, b = MOD, u = 1, v = 0;
+    while (b > 0) {
+      int t = a / b;
+      a -= t * b;
+      swap(a, b);
+      u -= t * v;
+      swap(u, v);
+    }
+    return u;
+  }
+
+  friend istream& operator>>(istream& is, mint& m) {
+    long long t;
+    is >> t;
+    m = t;
+    return is;
+  }
+  friend ostream& operator<<(ostream& os, const mint& m) {
+    return os << m.x;
+  }
+  int to_int() const {
+    return x;
+  }
+};
+
+mint operator+(long long x, const mint& m) {
+  return mint(x) + m;
+}
+mint operator-(long long x, const mint& m) {
+  return mint(x) - m;
+}
+mint operator*(long long x, const mint& m) {
+  return mint(x) * m;
+}
+mint operator/(long long x, const mint& m) {
+  return mint(x) / m;
+}
+
+int main() {
+  int n;
+  lint m;
+  cin >> n >> m;
+  vector<int> a(n);
+  rep (i, n) cin >> a[i];
+
+  a.emplace_back(1);
+  n++;
+
+  lint sum = accumulate(all(a), 0LL);
+
+  vector<mint> dp(2 * sum + 1);
+  dp[0] = 1;
+  rep (_, 60) {
+    rep (i, n) {
+      rrep (x, a[i], 2 * sum + 1) {
+        dp[x] += dp[x - a[i]];
+      }
+    }
+
+    vector<mint> ndp(2 * sum + 1);
+    rep (x, 2 * sum + 1) {
+      if (x % 2 == m % 2) ndp[x / 2] = dp[x];
+    }
+    dp = move(ndp);
+    m /= 2;
+  }
+  cout << dp[0] << endl;
+
+  return 0;
+}
