949f96ac1349c2f4fdb3a5257ca529d5ac187d2b
fura2 <yamazoe0577@gmail.com>
2026-04-20 02:19:15 +0900

    add ABC454 F

---
+++ atcoder/abc/abc454/f.cpp
@@ -0,0 +1,90 @@
+#ifndef TEMPLATE_HPP
+#define TEMPLATE_HPP
+
+#pragma GCC optimize("Ofast")
+#pragma GCC optimize("unroll-loops")
+
+#include <bits/stdc++.h>
+
+// clang-format off
+#define impl_overload4(a, b, c, d, e, ...) e
+
+#define impl_rep4(i, a, b, c) for (int i = (a); i < (b); i += (c))
+#define impl_rep3(i, a, b) impl_rep4(i, a, b, 1)
+#define impl_rep2(i, n) impl_rep3(i, 0, n)
+#define impl_rep1(n) for (int _ = 0; _ < (n); ++_)
+#define rep(...) impl_overload4(__VA_ARGS__, impl_rep4, impl_rep3, impl_rep2, impl_rep1)(__VA_ARGS__)
+
+#define impl_rrep4(i, a, b, c) for (int i = (b) - 1; i >= (a); i -= (c))
+#define impl_rrep3(i, a, b) impl_rrep4(i, a, b, 1)
+#define impl_rrep2(i, n) impl_rrep3(i, 0, n)
+#define rrep(...) impl_overload4(__VA_ARGS__, impl_rrep4, impl_rrep3, impl_rrep2)(__VA_ARGS__)
+
+#define all(v) std::begin(v), std::end(v)
+
+template<typename T>
+constexpr int bit(T x, unsigned int k) { return (x >> k) & 1; }
+
+template<typename T>
+constexpr bool chmax(T& a, const T& b) { return a < b ? a = b, true : false; }
+template<typename T>
+constexpr bool chmin(T& a, const T& b) { return a > b ? a = b, true : false; }
+
+constexpr void yesno(bool b) { std::cout << (b ? "Yes" : "No") << "\n"; }
+constexpr void yes() { yesno(true); }
+constexpr void no() { yesno(false); }
+
+struct Setup {
+  Setup() {
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+    std::cout << std::fixed << std::setprecision(10);
+  }
+} setup;
+// clang-format on
+
+#ifdef LOCAL
+#include "template_local.hpp"
+#else
+#define show(...) ((void)0)
+#endif
+
+using lint = long long;
+
+using namespace std;
+
+#endif // TEMPLATE_HPP
+
+constexpr lint INF = 1LL << 61;
+
+void solve() {
+  int n;
+  lint m;
+  cin >> n >> m;
+  vector<lint> a(n);
+  rep (i, n) cin >> a[i];
+
+  vector<lint> b(n / 2 + 2); // 両端に 0 を入れておく
+  rep (i, n / 2) b[i + 1] = (a[i] - a[n - i - 1] + m) % m;
+
+  vector<lint> d(n / 2 + 1);// b の差分
+  rep (i, n / 2 + 1) d[i] = (b[i + 1] - b[i] + m) % m;
+  sort(all(d));
+
+  vector<lint> cum(n / 2 + 2);
+  rep (i, n / 2 + 1) cum[i + 1] = cum[i] + d[i];
+  rep (i, n / 2 + 2) {
+    if (cum[i] == (n / 2 + 1 - i) * m - (cum[n / 2 + 1] - cum[i])) {
+      cout << cum[i] << endl;
+      return;
+    }
+  }
+  assert(false);
+}
+
+int main() {
+  int t;
+  cin >> t;
+  rep (_, t) solve();
+  return 0;
+}
